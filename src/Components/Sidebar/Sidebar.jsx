6e6f3f55a1e4616d233889213b6cfe5c0118b774
Sushil <56823375+Sushil456@users.noreply.github.com>
2023-07-11 16:28:20 +0530

    Sidebar With material icons added

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import './Sidebar.scss'
+import PersonIcon from '@mui/icons-material/Person';
+import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LowPriorityIcon from '@mui/icons-material/LowPriority';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import FitbitIcon from '@mui/icons-material/Fitbit';
+import LogoutIcon from '@mui/icons-material/Logout';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
 
 
 export const Sidebar = () => {
@@ -10,21 +22,66 @@ export const Sidebar = () => {
         <hr />
         <div className="center">
             <ul>
+                <p className="title">MAIN</p>
                 <li>
+                    <DashboardCustomizeIcon/>
                     <span>Dashboard</span>
                 </li>
+                <p className="title">LISTS</p>
                 <li>
-                    <span>Dashboard</span>
+                    <PersonIcon  />
+                    <span>Users</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <AccountBoxIcon/>
+                    <span>Products</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <ShoppingCartIcon/>
+                    <span>Orders</span>
+                </li>
+                <li>
+                    <LocalShippingIcon/>
+                    <span>Delivery</span>
+                </li>
+                <p className="title">USEFUL</p>
+                <li>
+                    <QueryStatsIcon/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <NotificationsActiveIcon/>
+                    <span>Notifications</span>
+                </li>
+                <p className="title">SERVICE</p>
+                <li>
+                    <HealthAndSafetyIcon/>
+                    <span>System Health</span>
+                </li>
+                <li>
+                    <FitbitIcon/>
+                    <span>Logs</span>
+                </li>
+                <li>
+                    <SettingsIcon/>
+                    <span>Settings</span>
+                </li>
+                <p className="title">USER</p>
+                <li>
+                    <LowPriorityIcon/>
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <LogoutIcon/>
+                    <span>Logout</span>
                 </li>
             </ul>
         </div>
-        <div className="bottom">color options</div>
+        <div className="bottom">
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+        </div>
     </div>
   )
 }
